4be6b5fadff1199159bdf85a374b2eab60de0e45
Matthew Odle <odle.matthew@gmail.com>
2019-05-03 16:27:24 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,25 @@
+import ast
+import threading
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options  
-import threading
 
-users = ['Chihemihe', 'Tikamajere', 'Kitiaramatar', 'Cheeheemeehee', 'Amomapilla', 'Lauralanthalesa', 'Crowmane']
+def get_users():
+    f = open("users.txt", "r")
+    users = f.read().split("\n")
+    return users
+
 
 def scrape(user):
+    file_path = "output/{}".format(user)
+    soup = get_soup(user)
+    current_sales = parse_sales(soup, user)
+    old_sales = get_previous(file_path)
+    write_new(current_sales, file_path)
+    do_diff(old_sales, current_sales)
+
+
+def get_soup(user):
     url = 'https://www.ffxiah.com/player/Asura/{}'.format(user)
     options = Options()
     options.headless = True
@@ -13,42 +27,63 @@ def scrape(user):
     browser.get(url)
     source = browser.page_source
     browser.close()
+    return BeautifulSoup(source, features="html.parser")
 
-    soup = BeautifulSoup(source, features="html.parser")
 
+def parse_sales(soup, user):
     sales_data = soup.find_all(id='player-sales')
-    #print(sales_data)
-    sales_data_tables = soup.find_all('table')
+    table = soup.find_all('table')[0]
 
-    for table in sales_data_tables:
-        table_body = table.find('tbody')
-        rows = table_body.find_all('tr')
-        output = []
-        for row in rows:
-            cols = row.find_all('td')
-            cols = [ele.text.strip() for ele in cols]
-            output.append([ele for ele in cols if ele])
+    diff_count = 0
 
-        start_index = 0
-        for i, o in enumerate(output, start=0):
-            if o and 'Price History' in o[0]:
-                start_index = i
-                break
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    output = []
 
-        file_path = "output/{}".format(user)
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        output.append([ele for ele in cols if ele])
+
+    print (output[0])
+    if user not in output[0][0]:
+        print ("no data for user {}".format(user))
+
+    start_index = 0
+    for i, o in enumerate(output, start=0):
+        if o and 'Price History' in o[0]:
+            start_index = i
+            break
+
+    new_contents = output[start_index+2:start_index+12]
+    new_joined = []
+    for entry in new_contents:
+        new_joined.append(", ".join(entry))
+    return new_joined
+
+
+def get_previous(file_path):
+    try:
         f = open(file_path, "r")
-        old_contents = f.read()
+        return ast.literal_eval(f.read())
+    except:
+        return []
 
-        new_contents = output[start_index+2:start_index+12]
-        new_joined = []
-        for entry in new_contents:
-            new_joined.append(", ".join(entry))
-        
-        f = open(file_path, "w+")
-        f.write(str(new_joined))
-        break
 
-for user in users:
-    scrape_thread = threading.Thread(target=scrape, args=(user,))
-    scrape_thread.start()
-    scrape_thread.join()
+def write_new(contents, file_path):
+    f = open(file_path, "w+")
+    f.write(str(contents))
+
+
+def do_diff(old, new):
+    list_diff = [i for i in old + new if i not in old]
+    if list_diff:
+        print ("{} new sales:\ncount: {}\n{}".format(user, len(list_diff), list_diff))
+
+
+if __name__ == "__main__":
+    users = get_users()
+    for user in users:
+        scrape_thread = threading.Thread(target=scrape, args=(user,))
+        scrape_thread.start()
+        scrape_thread.join()
